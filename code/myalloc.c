3f943012da4614058329f1fb6e86f5549ab8b30a
lachlanmerrill <ljam@st-andrews.ac.uk>
2019-10-14 15:44:16 +0100

    Works with coalescing

--- code/myalloc.c
+++ code/myalloc.c
@@ -77,13 +77,9 @@ void *myalloc(int size) {
 
 	}
 
+	printf("%ld\n", (long int)addressable);
+
 	struct prefooter* first_free = (struct prefooter*) addressable;
-
-
-	printf("Assigned header at %ld\n", (long int) (curr_header - (struct header*) addressable));
-	printf("Assigned footer %ld\n", (long int) (curr_footer - (struct header*) addressable));
-	printf("Assigned prefooter %ld\n", (long int) (prefooter - (struct prefooter*) addressable));
-	printf("Header size is %d\n", curr_header->size);
 
 	// Word align by ensuring size is divisible by four
 	size += size%WORD_SIZE;
@@ -119,6 +115,14 @@ void *myalloc(int size) {
 	struct header* old_footer = curr_footer;
 
 	prefooter = (struct prefooter*) curr_footer - HEADER_SIZE;
+
+	printf("First free block at %d\n", first_free->next);
+
+
+	printf("Assigned header at %ld\n", (long int) (curr_header - (struct header*) addressable));
+	printf("Assigned footer %ld\n", (long int) (curr_footer - (struct header*) addressable));
+	printf("Assigned prefooter %ld\n", (long int) (prefooter - (struct prefooter*) addressable));
+	printf("Header size is %d\n", curr_header->size);
 
 	int fail_flag = 1;
 
@@ -171,7 +175,7 @@ void *myalloc(int size) {
 		prefooter->next = next_header - (struct header*) addressable - HEADER_SIZE;
 
 		if (fail_flag) {
-			first_free->next = next_header - (struct header*) addressable - HEADER_SIZE;
+			first_free->next = next_header - (struct header*) addressable;
 		}
 
 	} else {
@@ -193,17 +197,24 @@ void *myalloc(int size) {
 }
 
 struct prefooter* find_prev_prefooter(struct header* search_header) {
-	struct header* curr_header = (struct header*) addressable;
+	struct prefooter* first_free = (struct prefooter*) addressable;
 
-	while (curr_header->active == FIELD_ACTIVE && curr_header->last == FIELD_NOTLAST) {
-		curr_header += curr_header->size;
-	}
+	struct header* curr_header = (struct header*) addressable + first_free->next;
 
 	if (curr_header >= search_header) {
-		return (struct prefooter*) addressable;
+		return first_free;
 	}
 
+	struct prefooter* curr_prefooter = (struct prefooter*) curr_header + curr_header->size - 2;
+	struct prefooter* prev_prefooter = curr_prefooter;
 
+	while (curr_header < search_header && curr_header->last == FIELD_NOTLAST) {
+		curr_header = (struct header*) addressable + curr_prefooter->next;
+		prev_prefooter = curr_prefooter;
+		curr_prefooter = (struct prefooter*) curr_header + curr_header->size;
+	}
+
+	return prev_prefooter;
 }
 
 
@@ -216,15 +227,18 @@ void myfree(void *ptr)	{
 	struct header* curr_footer = curr_header + curr_header->size - HEADER_SIZE;
 
 	struct prefooter* curr_prefooter = (struct prefooter*) curr_footer - HEADER_SIZE;
-	curr_prefooter->next = (int) (curr_header - (struct header*) addressable);
 
 	// Set header and footer active fields to inactive
 	curr_footer->active = FIELD_INACTIVE;
 	curr_header->active = FIELD_INACTIVE;
 
-	printf("Set current block to inactive.\n");
+	// printf("Set current block to inactive.\n");
+	//
+	// printf("%ld\n", (long int) curr_header - (long int) addressable);
+	// printf("Size: %d\nActive: %d\nLast: %d\n", curr_header->size, curr_header->active, curr_header->last);
 
-	printf("%ld, %ld\n", (long int) curr_header, (long int) addressable);
+	// printf("%ld\n", (long int) curr_footer - (long int) addressable);
+	// printf("Size: %d\nActive: %d\nLast: %d\n", curr_footer->size, curr_footer->active, curr_footer->last);
 
 	int coal_for = 0;
 	int coal_back = 0;
@@ -239,11 +253,15 @@ void myfree(void *ptr)	{
 	if (curr_header != (struct header*) addressable + HEADER_SIZE) {
 		prev_footer = curr_header - HEADER_SIZE;
 
+		// printf("owo\n");
+
 		if (prev_footer->active == FIELD_ACTIVE) {
 
 			coal_back = 0;
 
 		} else if (prev_footer->active == FIELD_INACTIVE) {
+
+			// printf("what's this\n");
 
 			coal_back = 1;
 			prev_header = prev_footer - prev_footer->size + HEADER_SIZE;
@@ -270,30 +288,69 @@ void myfree(void *ptr)	{
 
 		}
 
+
+		// printf("%ld\n", (long int) next_header - (long int) addressable);
+		// printf("Size: %d\nActive: %d\nLast: %d\n", next_header->size, next_header->active, next_header->last);
+
 	} else {
 
 		coal_for = 0;
 
 	}
 
+
+
 	if (!coal_for && !coal_back) {
 		// No coalescing required
 
+		// printf("No coalescing\n");
+
+		struct prefooter* prev_prefooter = find_prev_prefooter(curr_header);
+		struct prefooter* curr_prefooter = (struct prefooter*) curr_footer - HEADER_SIZE;
+
+		curr_prefooter->next = prev_prefooter->next;
+		prev_prefooter->next = curr_header - (struct header*) addressable;
+
 
 	} else if (coal_for && coal_back) {
+
+		// printf("For and back\n");
 		// Forwards and backwards required
 		prev_header->size = prev_header->size + curr_header->size + next_header->size;
 		next_footer->size = prev_header->size;
 
 	} else if (coal_for) {
+
+		// printf("For only\n");
 		// Forwards only required
 
+		struct prefooter* prev_prefooter = find_prev_prefooter(curr_header);
+		prev_prefooter->next = curr_header - (struct header*) addressable;
+
+		curr_header->size += next_header->size;
+		next_footer->size = curr_header->size;
 
 	} else if (coal_back) {
 		// Backwards only required
 
+		// printf("Back only\n");
+
+		prev_header->size += curr_header->size;prev_header->size;
+
+		curr_footer->size = prev_header->size;
+
+		struct prefooter* prev_prefooter = (struct prefooter*) prev_footer - HEADER_SIZE;
+		struct prefooter* curr_prefooter = (struct prefooter*) curr_footer - HEADER_SIZE;
+
+		curr_prefooter->next = prev_prefooter->next;
+
+		// printf("%ld: %d\n", prev_header - (struct header*) addressable, prev_header->size);
 
 	}
+
+	struct prefooter* first_free = (struct prefooter*) addressable;
+
+	printf("First free block now at %d\n", first_free->next);
 
 
 
--- code/test.c
+++ code/test.c
@@ -8,17 +8,19 @@
 
 void main()
 {
+
+    printf("Test 1\n");
     printf("Allocating 1024 * sizeof(int)... \n");
 
     int* ptr = myalloc(1024*sizeof(int));
     ptr[0] = 42;
     ptr[1023] = 25;
 
-    printf("Value 0 is %d\nValue 1023 is %d\n", ptr[0], ptr[1023]);
-
     int* ptr2 = myalloc(128*sizeof(int));
     ptr2[0] = 69;
     ptr2[127] = 420;
+    printf("In first array:\nValue 0 is %d\nValue 1023 is %d\n", ptr[0], ptr[1023]);
+
 
     printf("In second array:\nValue 0 is %d\nValue 127 is %d\n", ptr2[0], ptr2[127]);
 
@@ -28,5 +30,26 @@ void main()
     myfree(ptr2);
     printf("Second pointer freed\n");
 
-    printf("Yay!\n");
+    printf("Test 1 passed.\n");
+    printf("Test 2\n");
+
+    ptr = myalloc(1024*sizeof(int));
+    printf("First pointer allocated.\n");
+    ptr2 = myalloc(512*sizeof(int));
+
+    printf("Memory allocated.\n");
+
+    ptr[0] = 100;
+    ptr[1023] = 25;
+    ptr2[0] = 400;
+    ptr2[511] = 50;
+
+    printf("Values assigned.\n");
+
+    myfree(ptr2);
+    myfree(ptr);
+
+    printf("Test 2 passed.\n");
+
+    printf("Done.\n");
 }
