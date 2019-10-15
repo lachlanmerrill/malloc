690d724e158cc9744b226e484b95e939b3d5c4f9
lachlanmerrill <ljam@st-andrews.ac.uk>
2019-10-15 10:11:20 +0100

    Ran all tests on laptop. All passed.

--- code/myalloc.c
+++ code/myalloc.c
@@ -15,7 +15,7 @@
 #define WORD_SIZE 4
 #define HEADER_SIZE 1
 #define PAGE_SIZE 4096
-#define INIT_SIZE 512
+#define INIT_SIZE 4096
 
 struct header { // header is exactly 32 bits (4 bytes, 1 word)
 	int size : 30; // Size field takes 31 bits
